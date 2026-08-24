7abc9df952e0c6192cdd74e6912e2d02fe48a675
Андрей Хлапов <bclxl@mail.ru>
2026-08-24 17:55:11 +0300

    Final_work_Async-YaCut_v.2

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ class Config(object):
     """Конфигурационные данные проекта."""
 
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
     DISK_TOKEN = os.getenv('DISK_TOKEN')
